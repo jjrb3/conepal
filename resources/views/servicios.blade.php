7063255017ad6c4f14920301ad69c140fdfba6d6
IngAlvaro-PC\Ing. Alvaro <alvaroepm1994@gmail.com>
2017-01-30 09:32:04 -0500

    ingresa modulo de servicios

---
+++ resources/views/servicios.blade.php
@@ -0,0 +1,60 @@
+@extends('tema.usuario')
+
+@section('content')
+	<!-- banner -->
+	<div class="inside-banner">
+		<div class="container">
+			<span class="pull-right"><a href="inicio">Home</a> / Servicios</span>
+			<h2>Servicios</h2>
+		</div>
+	</div>
+	<!-- banner -->
+
+
+	<div class="container">
+		<div class="spacer">
+			<div class="row contact">
+				<div class="col-lg-6 col-sm-6 ">
+				<span class="glyphicon glyphicon-folder-open"> SERVICIOS INMOBILIARIOS</span>
+                  <ul>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Compra</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Venta</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Arriendos</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Seguros</span></li>
+                  </ul>
+
+                  <span class="glyphicon glyphicon-folder-open"> CONSTRUCCIÓN</span>
+                  <ul>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Creación de proyecto</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Remodelación</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Construcción</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Administración y logística de proyectos</span></li>
+                  </ul>
+
+
+				</div>
+			
+ 					<div class="col-lg-6 col-sm-6 ">
+				<span class="glyphicon glyphicon-folder-open"> RENDER</span>
+                  <ul>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Videos render</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Imagenes render</span></li>
+                  </ul>
+
+                  <span class="glyphicon glyphicon-folder-open"> Asesoría Juridica</span>
+                  <ul>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Permisos de construcciones</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Estudio de titulo inmobiliario</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Elaboración de contratos de venta y arriendos</span></li>
+                  	<li><span class="glyphicon glyphicon-ok-circle">Sucesiones</span></li>
+                  </ul>
+
+
+				</div>
+
+			</div>
+
+
+		</div>
+	</div>
+@endsection
